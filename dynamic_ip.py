6afa499dfa0daf04c227bb4199f33e477b969d24
Leechael Yim <yanleech@gmail.com>
2012-06-07 23:17:39 +0800

    BUGFIX: buggy cache.

--- dynamic_ip.py
+++ dynamic_ip.py
@@ -10,6 +10,7 @@ Usage: ./dynamic_ip.py [record_id]
 import re
 import os
 from zlib import crc32
+import time
 import requests
 from dnspod_lib import *
 
@@ -35,15 +36,18 @@ if __name__ == '__main__':
   if os.path.exists('/tmp/dnspod_%s' % filename):
     old_ip = None
     with open('/tmp/dnspod_%s' % filename, 'r') as f:
-      old_ip = f.read().strip()
-    if old_ip and old_ip == ip:
-      exit(0)
+      data = f.read().strip().split(' ')
+      expiry = int(data[0])
+      old_ip = data[1]
+    if time.time() <= expiry:
+      if old_ip and old_ip == ip:
+        exit(0)
 
   record = get_record(pattern)
   if record:
     update_record_ip(record, ip)
     with open('/tmp/dnspod_%s' % filename, 'w') as f:
-      f.write(ip)
+      f.write("%s %s" % (time.time() + 600, ip))
     print "Updated: %s now point to IP %s" % (pattern, ip)
     exit(0)
 
