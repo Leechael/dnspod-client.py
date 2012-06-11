d550ca17799847f68d4362f45d5afbbfb9390b24
Leechael Yim <yanleech@gmail.com>
2012-06-11 18:49:07 +0800

    Refactor: use gevent spawn get_ip worker, and refactor.

--- dynamic_ip.py
+++ dynamic_ip.py
@@ -11,8 +11,17 @@ import re
 import os
 from zlib import crc32
 import time
+import logging
+
+import gevent
+from gevent import monkey
+monkey.patch_all()
+
 import requests
 from dnspod_lib import *
+
+
+REGEX_IP=re.compile('(\d+\.\d+\.\d+\.\d+)')
 
 
 def crc32b (data):
@@ -22,10 +31,46 @@ def crc32b (data):
   return hashed
 
 
-def get_ip_by_dyndns ():
-  resp = requests.get('http://checkip.dyndns.org/')
-  ip = re.findall('(\d+\.\d+\.\d+\.\d+)', resp.content)
-  return ip[0]
+def read_cache (key):
+  fn = '/tmp/dnspod_%s' % crc32b(key)
+  if os.path.exists(fn):
+    with open(fn, 'r') as f:
+      data = f.read().strip().split(' ')
+      print data
+      if data and data[0]:
+        expiry = int(data[0])
+        if time.time() <= expiry:
+          return data[1]
+  return None
+
+
+def write_cache (key, value, expiry=3600):
+  fn = '/tmp/dnspod_%s' % crc32b(key)
+  with open(fn, 'w') as f:
+    f.write('%d %s' % (time.time()+expiry, value))
+    return True
+  return False
+
+
+def fetch (url):
+  resp = requests.get(url)
+  if resp.ok:
+    ips = REGEX_IP.findall(resp.content)
+    return ips[0]
+  return None
+
+
+def get_ip (timeout=5):
+  urls = (
+      'http://checkip.dyndns.org/',
+      'http://city.ip138.com/city.asp',
+      )
+  jobs = [gevent.spawn(fetch, url) for url in urls]
+  gevent.joinall(jobs, timeout=timeout)
+  for ip in [job.value for job in jobs]:
+    if ip:
+      return ip
+  return None
 
 
 if __name__ == '__main__':
@@ -34,27 +79,25 @@ if __name__ == '__main__':
     print __doc__
 
   pattern = argv[1]
+  logging.basicConfig(format='[%(asctime)-15s] [%(name)s] %(message)s',
+      filename='update.log', level=logging.DEBUG)
+  logger = logging.getLogger('dynamic_ip')
 
-  ip = get_ip_by_dyndns()
-  filename = crc32b(pattern)
-  if os.path.exists('/tmp/dnspod_%s' % filename):
-    old_ip = None
-    with open('/tmp/dnspod_%s' % filename, 'r') as f:
-      data = f.read().strip().split(' ')
-      expiry = int(data[0])
-      old_ip = data[1]
-    if time.time() <= expiry:
-      if old_ip and old_ip == ip:
-        exit(0)
+  ip = get_ip()
+  if not ip:
+    logger.critical('IP detects fault.')
+    exit(1)
 
-  if True:
-  #record = get_record(pattern)
-  #if record:
-    #update_record_ip(record, ip)
-    with open('/tmp/dnspod_%s' % filename, 'w') as f:
-      f.write("%s %s" % (time.time() + 600, ip))
-    print "Updated: %s now point to IP %s" % (pattern, ip)
+  prev = read_cache(pattern)
+  if prev and prev == ip:
+    logger.info('Lastest IP still fresh: %s' % ip)
     exit(0)
 
-  print "Do nothing."
-  exit(1)
+  record = get_record(pattern)
+  if not record:
+    logger.critical('Pattern `%s` has not match any exists record.' % pattern)
+    exit(1)
+
+  update_record_ip(record, ip)
+  write_cache(pattern, ip)
+  logging.info("Updated: %s now point to IP %s" % (pattern, ip))
