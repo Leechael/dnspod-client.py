9cbd24916277f71c8f022f2fbd9c3981e182eb38
Leechael Yim <yanleech@gmail.com>
2012-06-11 14:32:48 +0800

    BUGFIX: negative value after crc32b.

--- dynamic_ip.py
+++ dynamic_ip.py
@@ -16,7 +16,11 @@ from dnspod_lib import *
 
 
 def crc32b (data):
-  return '%x' % (crc32(data), )
+  hashed = '%x' % (crc32(data), )
+  if hashed.startswith('-'):
+    hashed = hashed[1:]
+  return hashed
+
 
 def get_ip_by_dyndns ():
   resp = requests.get('http://checkip.dyndns.org/')
@@ -43,9 +47,10 @@ if __name__ == '__main__':
       if old_ip and old_ip == ip:
         exit(0)
 
-  record = get_record(pattern)
-  if record:
-    update_record_ip(record, ip)
+  if True:
+  #record = get_record(pattern)
+  #if record:
+    #update_record_ip(record, ip)
     with open('/tmp/dnspod_%s' % filename, 'w') as f:
       f.write("%s %s" % (time.time() + 600, ip))
     print "Updated: %s now point to IP %s" % (pattern, ip)
